7b8c97a5f83a9ce0113d0390b2c9b06cae79d388
Crowdbotics <team@crowdbotics.com>
2024-05-07 19:55:43 +0000

    Initial commit

---
+++ backend/rideshareapplicatio_47849/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for rideshareapplicatio_47849 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "rideshareapplicatio_47849.settings"
+)
+
+application = get_wsgi_application()
